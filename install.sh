047f7feb9052164a7a01e705e2e85bf8bfda134e
Jake Brinkmann <jakebrinkmann@gmail.com>
2019-02-07 03:14:46 -0600

    fix(docker): remove privilege escalation

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
+#!/usr/bin/env bash
+# Installs kubernetes from the Google repo
 
+# All must be run as root
+[ $(/usr/bin/id -u) -ne 0 ] \
+    && echo 'Must be run as root!' \
+    && exit 1
 
-sudo cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+# Add Google K8s repository
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
 baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
@@ -10,20 +17,33 @@ repo_gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
 
-sudo setenforce 0
-sudo yum install -y kubelet kubeadm kubectl
-sudo systemctl enable kubelet && systemctl start kubelet
+# Turn off SELinux Security and Swap
+setenforce 0
+swapoff -a
 
-sudo cat <<EOF >  /etc/sysctl.d/k8s.conf
+# Install K8s, as well as CLI utilities
+yum install -y \
+    kubelet \
+    kubeadm \
+    kubectl
+
+# Enable Kubelet to start on boot (if in an init system)
+[ ! -f /.dockerenv ] \
+  && systemctl enable kubelet \
+  && systemctl start kubelet
+
+# Configure NetworkPlugin to connect containers in Linux Bridge
+cat <<EOF >  /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 EOF
-sudo sysctl --system
-sudo sysctl net.bridge.bridge-nf-call-iptables=1
 
-sudo swapoff -a
+# Update kubelet
+sysctl --system
+sysctl net.bridge.bridge-nf-call-iptables=1
 
-mkdir -p $HOME/.kube
+# Download Kompose utility (convert docker-compose YAML into K8s YAML)
+curl -fsSL -o /usr/local/bin/kompose \
+  https://github.com/kubernetes/kompose/releases/download/v1.13.0/kompose-linux-amd64 \
+  && chmod +x /usr/local/bin/kompose
 
-sudo curl -L https://github.com/kubernetes/kompose/releases/download/v1.13.0/kompose-linux-amd64 -o /usr/local/bin/kompose
-sudo chmod +x /usr/local/bin/kompose
